2b45224d33384aaaf4dd648a5be03b29d27e456e
terrign <vajipi@gmail.com>
2022-10-17 21:41:24 +0300

    commit

--- src/stack.js
+++ src/stack.js
@@ -12,22 +12,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
-class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class Stack {
+  constructor () {
+    this.stack = []
+  }
+  push(v) {
+      this.stack.push(v)
+      return this
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+      return this.stack.pop()
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+      return this.stack[this.stack.length-1]
   }
 }
+
 
 module.exports = {
   Stack
